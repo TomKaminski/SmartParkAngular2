cc0b450353a59582c6a1d97faf23355cc795098b
Tomasz Kamiński <tomasz.kaminski@euvic.pl>
2016-11-09 13:22:31 +0100

    app header

--- src/app/portal-header/portal-header.component.ts
+++ src/app/portal-header/portal-header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-portal-header',
   templateUrl: './portal-header.component.html',
-  styleUrls: ['./portal-header.component.scss']
+  styleUrls: ['./portal-header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PortalHeaderComponent implements OnInit {
 
--- src/app/portal-main/portal-main.component.ts
+++ src/app/portal-main/portal-main.component.ts
@@ -1,10 +1,11 @@
 import { SmartparkApiService } from '../api/smartpark-api.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-portal-main',
   templateUrl: './portal-main.component.html',
-  styleUrls: ['./portal-main.component.scss']
+  styleUrls: ['./portal-main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PortalMainComponent implements OnInit {
 
