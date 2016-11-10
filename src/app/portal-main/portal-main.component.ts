80938065a3152ecc627c5e59990786a053b7e330
Tomasz Kamiński <tkaminski93@gmail.com>
2016-11-10 12:57:40 +0100

    css cleanup...

--- src/app/portal-main/portal-main.component.ts
+++ src/app/portal-main/portal-main.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-portal-main',
   templateUrl: './portal-main.component.html',
-  styleUrls: ['./portal-main.component.scss'],
+  styleUrls: ['./styles/dashboard.scss','./styles/portal-main.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class PortalMainComponent implements OnInit {
